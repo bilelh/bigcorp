1a4436536deae53f7c6d3a849bcda4471f361bb3
bilel <bilelh532@gmail.com>
2019-06-14 12:58:01 +0200

    TP18 REST & SPRING   Ecriture et Test du client REST OK

--- src/main/java/com/training/springcore/service/measure/SimulatedMeasureService.java
+++ src/main/java/com/training/springcore/service/measure/SimulatedMeasureService.java
@@ -8,10 +8,15 @@ import com.training.springcore.model.MeasureStep;
 import com.training.springcore.model.SimulatedCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -20,16 +25,35 @@ public class SimulatedMeasureService implements MeasureService <SimulatedCaptor>
     @Autowired
     BigcorpApplicationProperties properties;
 
+    private RestTemplate restTemplate;
+
+    public SimulatedMeasureService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.setConnectTimeout(Duration.ofSeconds(1)).build();
+    }
+
     @Override
     public List<Measure> readMeasures(SimulatedCaptor captor, Instant start, Instant end, MeasureStep step) {
         checkReadMeasuresAgrs(captor, start, end, step);
 
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromHttpUrl("http://localhost:8090/measures")
+                .path("")
+                .queryParam("start", start)
+                .queryParam("end", end)
+                .queryParam("min", captor.getMinPowerInWatt())
+                .queryParam("max", captor.getMaxPowerInWatt())
+                .queryParam("step", step.getDurationInSecondes());
+
+        Measure[] measures = this.restTemplate.getForObject(builder.toUriString(), Measure[].class);
+        return Arrays.asList(measures);
+
+        /*
         List<Measure> measures = new ArrayList<>();
         Instant current = start;
         while(current.isBefore(end)){
             measures.add(new Measure(current, properties.getMeasure().getDefaultSimulated(), captor));
             current = current.plusSeconds(step.getDurationInSecondes());
         }
-        return measures;
+        return measures;*/
     }
 }
--- src/test/java/com/training/springcore/service/measure/SimulatedMeasureServiceTest.java
+++ src/test/java/com/training/springcore/service/measure/SimulatedMeasureServiceTest.java
@@ -1,29 +1,45 @@
 package com.training.springcore.service.measure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.training.springcore.controller.dto.MeasureDto;
 import com.training.springcore.model.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.groups.Tuple.tuple;
 
 @RunWith(SpringRunner.class)
+@RestClientTest(SimulatedMeasureService.class)
 @ContextConfiguration(classes = {MeasureServiceConfigurationTest.class})
 public class SimulatedMeasureServiceTest {
 
     @Autowired
     private SimulatedMeasureService service;
+
+    @Autowired
+    private MockRestServiceServer server;
+
+    @Autowired
+    private ObjectMapper objectMapper;
     /**
      * Captor used in tests
      */
-    private SimulatedCaptor captor = new SimulatedCaptor("test", new Site("site"),50000, 100000);
+    private SimulatedCaptor captor = new SimulatedCaptor("test", new Site("site"),500000, 1000000);
     /**
      * Start instant used in tests
      */
@@ -55,6 +71,7 @@ public class SimulatedMeasureServiceTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("start must be before end");
     }
+    /*
     @Test
     public void readMeasures(){
         List<Measure> measures = service.readMeasures(captor, start, end,
@@ -92,5 +109,30 @@ public class SimulatedMeasureServiceTest {
                         "2018-09-02T19:00:00Z",
                         "2018-09-02T20:00:00Z",
                         "2018-09-02T21:00:00Z");
+    }*/
+
+    @Test
+    public void readMeasures() throws Exception {
+        Instant start = Instant.parse("2018-09-01T22:00:00Z");
+        Instant end = Instant.parse("2018-09-01T22:30:00Z");
+        List<MeasureDto> expectedMeasures = Arrays.asList(
+                new MeasureDto(Instant.parse("2018-09-01T22:00:00Z"), 1234),
+                new MeasureDto(Instant.parse("2018-09-01T22:30:00Z"), 4567)
+        );
+        String expectedJson = objectMapper.writeValueAsString(expectedMeasures);
+        String request = "http://localhost:8090/measures?start=2018-09-01T22:00:00Z&" +
+                "end=2018-09-01T22:30:00Z&min=500000&max=1000000&step=3600";
+        this.server.expect(MockRestRequestMatchers.requestTo(request))
+                .andRespond(MockRestResponseCreators.withSuccess(expectedJson,
+                MediaType.APPLICATION_JSON));
+        List<Measure> measures = service.readMeasures(captor, start, end,
+                MeasureStep.ONE_HOUR);
+        assertThat(measures).hasSize(2);
+        // And we have a value for each hour of the period
+        assertThat(measures)
+                .extracting("instant", "valueInWatt")
+                .containsExactly(tuple(Instant.parse("2018-09-01T22:00:00Z"),1234),
+                        tuple(Instant.parse("2018-09-01T22:30:00Z"),4567)
+                );
     }
 }
